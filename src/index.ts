37250909608a3c6adc070ce8587d22f29c793d07
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2026-01-07 14:38:27 +0100

    implement security middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from "cors";
 
 import subjectsRouter from "./routes/subjects";
+import securityMiddleware from "./middleware/security";
 
 const app = express();
 const PORT = 8000;
@@ -16,7 +17,9 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use('/api/subjects', subjectsRouter)
+app.use(securityMiddleware);
+
+app.use('/api/subjects', subjectsRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, welcome to the Classroom API!');
