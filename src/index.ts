5fd09724cce53d8bf139325694d8f10c4253a0d7
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2026-01-07 19:02:45 +0100

    ...

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import cors from "cors";
 
 import subjectsRouter from "./routes/subjects.js";
 import securityMiddleware from "./middleware/security.js";
+import {toNodeHandler} from "better-auth/node";
+import {auth} from "./lib/auth.js";
 
 const app = express();
 const PORT = 8000;
@@ -17,6 +19,8 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
