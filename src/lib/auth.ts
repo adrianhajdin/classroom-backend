ca270d730d886b4ff41040f0ce2bd2cdef7e696e
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2026-01-07 18:51:38 +0100

    add file extensions

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-
-import { db } from "../db.js";
+import { db } from "../db/index.js";
 import * as schema from '../db/schema/auth.js'
 
 export const auth = betterAuth({
